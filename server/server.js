200f80551a2ef6509e0bb4928de67c5f5bc3c6ab
nainglinaung <nainglinaung91@gmail.com>
2024-11-25 22:24:24 +0700

    chore: db update and adding new routes

--- server/server.js
+++ server/server.js
@@ -5,9 +5,11 @@ const port = process.env.PORT || 5000;
 
 const taskRouter = require('./app/task/task.route');
 const userRouter = require('./app/user/user.route');
+const organizationRouter = require('./app/organization/organization.route');
 
 app.use('/task', taskRouter);
 app.use('/user', userRouter);
+app.use('/organization', organizationRouter);
 
 
 if (process.env.NODE_ENV !== 'test') {
