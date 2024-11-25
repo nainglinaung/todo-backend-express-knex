a3524d6f2e1bdf1140088f7b7e2a9daf179795e4
nainglinaung <nainglinaung91@gmail.com>
2024-11-25 22:25:06 +0700

    chore: adding new middleware

--- server/app/user/user.route.js
+++ server/app/user/user.route.js
@@ -3,12 +3,14 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('./user.controller');
+const authMiddleware = require('../../middleware/auth.middleware');
+
 
 router.post('/', userController.create);
 router.post('/login', userController.login);
-router.put('/:id', userController.update);
-router.get('/', userController.getAll);
-router.get('/:id', userController.get);
-router.delete('/:id', userController.remove);
+router.put('/:id',authMiddleware, userController.update);
+router.get('/',authMiddleware, userController.getAll);
+router.get('/:id',authMiddleware, userController.get);
+router.delete('/:id',authMiddleware, userController.remove);
 
 module.exports = router;
