8ee1666db7563206bb9d370fe172eab450170c46
nainglinaung <nainglinaung91@gmail.com>
2024-11-25 22:26:24 +0700

    feat: assign and filtering with org id

--- server/app/task/task.controller.js
+++ server/app/task/task.controller.js
@@ -5,15 +5,20 @@ const prisma = new PrismaClient();
 
 const create = async (req, res) => {
   const { title, description } = req.body;
-  try {
+    try {
+      
+ 
     const task = await prisma.task.create({
       data: {
         title,
         description,
+        organizationId: req.user.organizationId,
+        userId: 1,
       },
     });
     res.status(201).json(task);
   } catch (error) {
+      console.error(error)
     res.status(500).json({ error: 'Failed to create task' });
   }
 };
@@ -21,9 +26,11 @@ const create = async (req, res) => {
 const update = async (req, res) => {
   const { id } = req.params;
   const { title, description, completed } = req.body;
-  try {
+    try {
+    
+
     const task = await prisma.task.update({
-      where: { id: parseInt(id) },
+      where: { id: parseInt(id), organizationId:req.user.organizationId },
       data: {
         title,
         description,
@@ -38,7 +45,7 @@ const update = async (req, res) => {
 
 const getAll = async (req, res) => {
   try {
-    const tasks = await prisma.task.findMany();
+    const tasks = await prisma.task.findMany({where:{organizationId:req.user.organizationId}});
     res.status(200).json(tasks);
   } catch (error) {
     res.status(500).json({ error: 'Failed to retrieve tasks' });
@@ -49,7 +56,7 @@ const get = async (req, res) => {
   const { id } = req.params;
   try {
     const task = await prisma.task.findUnique({
-      where: { id: parseInt(id) },
+      where: { id: parseInt(id), organizationId:req.user.organizationId },
     });
     if (task) {
       res.status(200).json(task);
@@ -65,7 +72,7 @@ const remove = async (req, res) => {
   const { id } = req.params;
   try {
     await prisma.task.delete({
-      where: { id: parseInt(id) },
+      where: { id: parseInt(id), organizationId:req.user.organizationId },
     });
     res.status(204).send();
   } catch (error) {
--- server/app/user/user.controller.js
+++ server/app/user/user.controller.js
@@ -48,7 +48,7 @@ const login = async (req, res) => {
         return res.status(401).json({ error: 'Invalid credentials' });
     }
     
-    const token = jwt.sign({ id: user.id, email: user.email }, process.env.JWT_SECRET, {
+    const token = jwt.sign({ id: user.id, email: user.email, organizationId:user.organizationId }, process.env.JWT_SECRET, {
         expiresIn: '1h',
     });
     
@@ -59,22 +59,28 @@ const login = async (req, res) => {
 
 const update = async (req, res) => {
   const { id } = req.params;
-    const { name, email, password } = req.body;
+    const { name, email, password,organizationId } = req.body;
     
     if (password) {
         password = await bcrypt.hash(password, 10);
     }
 
-  try {
-    const task = await prisma.user.update({
-      where: { id: parseInt(id) },
-      data: {
-        name,
-        email,
-        password,
-      },
-    });
-    res.status(200).json(task);
+    try {
+      
+        if (parseInt(id) !== req.user.id) { 
+            return res.status(403).json({ error: 'Forbidden' });
+        }
+
+        const user = await prisma.user.update({
+        where: { id: parseInt(id) },
+        data: {
+            name,
+            email,
+            password,
+            organizationId
+        },
+        });
+        return res.status(200).json(user);
   } catch (error) {
     res.status(500).json({ error: 'Failed to update user' });
   }
@@ -82,8 +88,8 @@ const update = async (req, res) => {
 
 const getAll = async (req, res) => {
   try {
-    const tasks = await prisma.user.findMany();
-    res.status(200).json(tasks);
+    const users = await prisma.user.findMany();
+    res.status(200).json(users);
   } catch (error) {
     res.status(500).json({ error: 'Failed to retrieve users' });
   }
@@ -92,11 +98,11 @@ const getAll = async (req, res) => {
 const get = async (req, res) => {
   const { id } = req.params;
   try {
-    const task = await prisma.user.findUnique({
+    const user = await prisma.user.findUnique({
       where: { id: parseInt(id) },
     });
-    if (task) {
-      res.status(200).json(task);
+    if (user) {
+      res.status(200).json(user);
     } else {
       res.status(404).json({ error: 'Task not found' });
     }
